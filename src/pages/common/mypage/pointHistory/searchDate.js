4641d32abe9b3a8610c2013551b397e4b134319a
tprud9412 <37810297+tprud9412@users.noreply.github.com>
2023-05-21 22:09:21 +0900

    포인트 히스토리 수정

--- src/pages/common/mypage/pointHistory/searchDate.js
+++ src/pages/common/mypage/pointHistory/searchDate.js
@@ -6,6 +6,7 @@ import {
   Button,
   Typography,
   Avatar,
+  Card,
 } from "@mui/material";
 
 import dayjs from "dayjs";
@@ -22,48 +23,50 @@ export const SearchDate = ({
   buttonSubmit,
 }) => {
   return (
-    <>
-      <Container>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={["DatePicker", "DatePicker"]}>
-            <Grid
-              container
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <Grid item xs={4}>
-                <DatePicker
-                  label=""
-                  value={firstDate}
-                  views={["year", "month", "day"]}
-                  onChange={(newValue) => setFirstDate(newValue)}
-                />
-              </Grid>
-              <Typography sx={{ ml: 1, mr: 1 }}> ~ </Typography>
-              <Grid item xs={4}>
-                <DatePicker
-                  label=""
-                  value={secondaryDate}
-                  views={["year", "month", "day"]}
-                  onChange={(newValue) => setSecondaryDate(newValue)}
-                />
-              </Grid>
-              <Grid item xs={2} sx={{ ml: 1 }}>
-                <Button
-                  size="medium"
-                  variant="contained"
-                  sx={{ width: "80px" }}
-                  onClick={buttonSubmit}
-                >
-                  검색
-                </Button>
-              </Grid>
-            </Grid>
-          </DemoContainer>
-        </LocalizationProvider>
-      </Container>
-    </>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Grid item xs={4}>
+          <DatePicker
+            label=""
+            value={firstDate}
+            views={["year", "month", "day"]}
+            onChange={(newValue) => setFirstDate(newValue)}
+          />
+        </Grid>
+        <Typography variant="body2" sx={{ ml: 1, mr: 1 }}>
+          {" "}
+          ~{" "}
+        </Typography>
+        <Grid item xs={4}>
+          <DatePicker
+            label=""
+            value={secondaryDate}
+            views={["year", "month", "day"]}
+            onChange={(newValue) => setSecondaryDate(newValue)}
+          />
+        </Grid>
+        <Grid item xs={2} sx={{ ml: 1 }}>
+          <Button
+            size="medium"
+            variant="outlined"
+            sx={{
+              width: "80px",
+              borderColor: "ButtonHighlight",
+              backgroundColor: "ButtonFace",
+              color: "ButtonText",
+            }}
+            onClick={buttonSubmit}
+          >
+            검색
+          </Button>
+        </Grid>
+      </Grid>
+    </LocalizationProvider>
   );
 };
